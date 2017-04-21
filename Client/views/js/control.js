28f97e3e934ab82537f11cc6ccc88fb32bd84634
Rikhardur Bjarni Einarsson <badulf96@gmail.com>
2017-04-21 10:40:07 +0000

    Refined the look of the UI

--- Client/views/js/control.js
+++ Client/views/js/control.js
@@ -3,7 +3,25 @@
 let directory = $('#directory').text();
 const port = require(directory + '/port.js');
 
+var CallManager = {
+  current: undefined,
+  set: (callback, time) => {
+    if (CallManager.current !== undefined) {
+      CallManager.kill();
+    }
+
+    CallManager.current = setTimeout(callback, time);
+  },
+  kill: () => {
+    if (CallManager.current !== undefined) {
+      clearTimeout(CallManager.current);
+      CallManager.current = undefined;
+    }
+  }
+};
+
 $(document).ready(() => {
+
   $('.element-on').click(() => {
     port.send('eo');
   });
@@ -22,9 +40,16 @@ $(document).ready(() => {
 
   $('.all-off').click(() => {
     port.send('ii');
+    CallManager.kill();
   });
 
-  var t = () => {
-    $('#coffee').get(0).switch_content('main');
-  }
+  var stop = () => {
+    port.send('ii');
+    CallManager.current = undefined;
+    $('#stop').get(0).switch_content('main');
+  };
+
+  $('.create-espresso').click(() => { CallManager.set(stop, 10000); });
+  $('.create-tea').click(() => { CallManager.set(stop, 8000); });
+  $('.create-brew').click(() => { CallManager.set(stop, 60000); });
 });
